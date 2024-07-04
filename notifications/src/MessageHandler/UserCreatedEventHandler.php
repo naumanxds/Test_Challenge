7b04ee6428dc85de835a9189bc215fa3b2d538d3
naumanxds <nauman.nasir919@gmail.com>
2024-07-04 18:23:47 +0500

    update consumer and add logger to write consumed object in the log file in notifications microservice

--- notifications/src/MessageHandler/UserCreatedEventHandler.php
+++ notifications/src/MessageHandler/UserCreatedEventHandler.php
@@ -3,15 +3,23 @@
 namespace App\MessageHandler;
 
 use App\Message\UserCreatedEvent;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 class UserCreatedEventHandler
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
 
     public function __invoke(UserCreatedEvent $event): void
     {
-        $logData = sprintf("User created: %s %s <%s>", $event->getFirstName(), $event->getLastName(), $event->getEmail());
+        $logData = sprintf("{{{ User created: %s %s <%s> }}}", $event->getFirstName(), $event->getLastName(), $event->getEmail());
         dump($logData);
+        $this->logger->log('NOTICE', $logData);
     }
 }
