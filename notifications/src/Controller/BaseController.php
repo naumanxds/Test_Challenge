eb5ff634f158b2ce139dcaacafb69773d6c12393
naumanxds <nauman.nasir919@gmail.com>
2024-07-04 20:35:49 +0500

    make a page to view logs in the frontend

--- notifications/src/Controller/BaseController.php
+++ notifications/src/Controller/BaseController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use Exception;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,5 +14,20 @@ class BaseController extends AbstractController
     public function index(): Response
     {
         return $this->render('base/index.html.twig');
+    }
+
+    #[Route('/logs', name: 'logs')]
+    public function logs(LoggerInterface $logger): Response
+    {
+        $logsData = '';
+        try {
+            $logsData = file_get_contents($this->getParameter('path.user_event_log'));
+        } catch (\Exception $e) {
+            $logger->error('User Event File not found');
+        }
+
+        $logsData = explode(PHP_EOL, $logsData);
+
+        return $this->render('base/logs.html.twig', [ 'logsData' => $logsData ]);
     }
 }
